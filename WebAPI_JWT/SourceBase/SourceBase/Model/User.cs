d57896d77c662a5e141c70c02a823d623f0b0d61
An Nguyen <annguyen3024@gmail.com>
2024-10-21 20:29:38 +0700

    full project

---
+++ WebAPI_JWT/SourceBase/SourceBase/Model/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SourceBase.Model
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
